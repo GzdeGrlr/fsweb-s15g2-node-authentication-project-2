31d4e67d8dfcec74ce4f13503b352831403b197b
Gözde G <gzde.gurler@gmail.com>
2023-02-14 20:40:57 +0300

    proje tamamlandi

--- api/server.js
+++ api/server.js
@@ -14,11 +14,16 @@ server.use(cors());
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
   });
+});
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "Not Found" });
 });
 
 module.exports = server;
